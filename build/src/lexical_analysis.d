fa0fd0d5a64502053766aeb7968a6bfd221be629
Artyom Kaplin <kaplinstu1@gmail.com>
2025-04-24 20:05:15 +0300

    empty

--- build/src/lexical_analysis.d
+++
@@ -1,8 +0,0 @@
-build/src/lexical_analysis.o: src/lexical_analysis.cpp \
- include/lexical_analysis.h include/diff_tree.h libs/logger/logger.h \
- include/file_read.h common/colors.h
-include/lexical_analysis.h:
-include/diff_tree.h:
-libs/logger/logger.h:
-include/file_read.h:
-common/colors.h:
