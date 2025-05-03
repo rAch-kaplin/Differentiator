50aff4aad2dc68268c94c38cc73300de5bb2f235
Kaplin Artyom <kaplinstu1@gmail.com>
2025-05-03 22:39:41 +0300

    start write partial derivatives, need make teylor

--- build/src/TeX_dump.d
+++
@@ -1,9 +0,0 @@
-build/src/TeX_dump.o: src/TeX_dump.cpp libs/logger/logger.h \
- include/diff_tree.h include/tree_func.h include/diff_tree.h \
- include/TeX_dump.h include/tree_func.h
-libs/logger/logger.h:
-include/diff_tree.h:
-include/tree_func.h:
-include/diff_tree.h:
-include/TeX_dump.h:
-include/tree_func.h:
--- libs/arg_parser/build/arg_parser.d
+++
@@ -1,2 +0,0 @@
-build/arg_parser.o: arg_parser.cpp arg_parser.h
-arg_parser.h:
